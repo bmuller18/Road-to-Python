b7ddaaf7241116a785ecfe02eaaf8b12f37f6aa8
Briant Muller <bmuller18@gmail.com>
2022-11-24 17:30:43 -0500

    Remove the dashed line

--- ShowText.py
+++ ShowText.py
@@ -18,15 +18,18 @@ root.title("My Main")
 x = root.winfo_screenwidth() // 2
 y = int(root.winfo_screenheight() * 0.1)
 root.geometry('500x600+' + str(x) + '+' + str(y))
+#root.attributes('-alpha',0.5)
 
 
 #Create TOP MENU BARNAV
 my_menu = Menu(root)
 root.config(menu=my_menu)
-file_menu = Menu(my_menu)
+file_menu = Menu(my_menu, tearoff="off")
 my_menu.add_cascade(label="WareHouse", menu= file_menu)
 file_menu.add_cascade(label="Add Item", command=AddItem)
-my_menu.add_cascade(label="Customer", menu=file_menu)
+file_menu.add_cascade(label="Delete Item")
+file_menu.add_cascade(label="List Items")
+my_menu.add_cascade(label="Customer")
 
 
 frame1 = tk.Frame(root, width=500, height=600, bg=backcolor)
