b01ee719646da537db41be819b61a0b420fb0eac
Briant Muller <bmuller18@gmail.com>
2022-11-23 22:37:56 -0500

    Add top menu

--- ShowText.py
+++ ShowText.py
@@ -1,20 +1,34 @@
 import tkinter as tk
+from tkinter import *
 from PIL import ImageTk
 
 #COLORS
 backcolor = "#8A82FB"
+backcolor2 = "#C977FD"
+
+def AddItem():
+    print("New item")
+    
+    
 root = tk.Tk()
 root.title("My Main")
+
+
 #root.eval("tk::PlaceWindow . center")
 x = root.winfo_screenwidth() // 2
 y = int(root.winfo_screenheight() * 0.1)
 root.geometry('500x600+' + str(x) + '+' + str(y))
 
-frame1 = tk.Frame(root, width=500, height=600, bg=backcolor)
-logo_image = ImageTk.PhotoImage(file="Assets/Logo.png")
 
-logo_widget = tk.Label(frame1, image=logo_image)
-logo_image.image = logo_image
-logo_widget.pack()
+#Create TOP MENU BARNAV
+my_menu = Menu(root)
+root.config(menu=my_menu)
+file_menu = Menu(my_menu)
+my_menu.add_cascade(label="WareHouse", menu= file_menu)
+file_menu.add_cascade(label="Add Item", command=AddItem)
+my_menu.add_cascade(label="Customer", menu=file_menu)
+
+
+frame1 = tk.Frame(root, width=500, height=600, bg=backcolor)
 frame1.pack()
 root.mainloop()
